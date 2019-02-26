a96a8a1d70259fc312240c5bee34c8713b49d24d
Asvør Rasmussen <asvo0011@easv365.dk>
2019-02-26 11:24:03 +0100

    refactored fxml and controller

--- src/ultimatetictactoebamt/UTTTController.java
+++ src/ultimatetictactoebamt/UTTTController.java
@@ -17,7 +17,7 @@ import javafx.scene.layout.GridPane;
  *
  * @author Theis
  */
-public class FXMLDocumentController implements Initializable
+public class UTTTController implements Initializable
 {
 
     @FXML
